201bc5954a662edd203c5555ddee4e44c373f576
Ayush Bhargav <ab.ayushbhargav0207@gmail.com>
2018-05-09 16:43:25 +0530

    Create problem5.py

---
+++ problem5.py
@@ -0,0 +1,19 @@
+primes = [2,3,5,7,11,13,17,19]
+factors = {str(num):0 for num in primes}
+for num in range(2,21):
+    for prime in primes:
+        if prime > num:
+            break
+        powers = 0
+        tmp = num
+        while(tmp % prime == 0):
+            tmp /= prime
+            powers += 1
+        if powers > factors[str(prime)]:
+            factors[str(prime)] = powers
+print(factors)
+ans = 1
+mul = [pow(int(n), p) for n, p in factors.items()]
+for m in mul:
+    ans *= m
+print(ans)
