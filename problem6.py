d108d68fdf9fda5bd6c64fc798aa354e066db889
Ayush Bhargav <ab.ayushbhargav0207@gmail.com>
2018-05-14 15:09:40 +0530

    Update problem6.py

--- problem6.py
+++ problem6.py
@@ -1,4 +1,9 @@
+import time
+start_time = time.time()
 s1 = sum([pow(x,2) for x in range(1, 101)])
 s2 = pow(sum(x for x in range(1, 101)), 2)
 
-print(s2 - s1)
+ans = s2 - s1
+diff_time = time.time() - start_time
+# 25164150 in 0.0 seconds
+print("{0} in {1} seconds".format(ans, diff_time))
