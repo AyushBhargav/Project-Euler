99adc480a11c51e1d637929be76a8711db486020
Ayush Bhargav <ab.ayushbhargav0207@gmail.com>
2018-05-14 15:28:48 +0530

    Update problem7.py

--- problem7.py
+++ problem7.py
@@ -1,6 +1,9 @@
+from timeit import default_timer as timer
+start_time = timer()
 primes = []
 num = 2
 prime_count = 0
+ans = -1
 while True:
     flag = True
     for prime in primes:
@@ -11,6 +14,10 @@ while True:
         prime_count += 1
         primes.append(num)
     if prime_count == 10001:
-        print(num)
+        ans = num
         break
     num += 1
+
+diff_time = timer()
+# 104743 in 26.408278233 seconds
+print("{0} in {1} seconds".format(ans, diff_time))
