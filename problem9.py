6e69c01d72d71368722a0e93826450c8201b5ef4
Ayush Bhargav <ab.ayushbhargav0207@gmail.com>
2018-05-14 15:42:56 +0530

    Update problem9.py

--- problem9.py
+++ problem9.py
@@ -1,11 +1,18 @@
+from timeit import default_timer as timer
+start_time = timer()
 arr = list(range(1, 1001))
+ans = -1
 for a in arr:
     flag = False
     for b in arr:
         c = math.sqrt(a**2 + b**2)
         if math.floor(c) == c and a+b+c == 1000:     
             flag = True
-            print("{0}*{1}*{2}->{3}".format(a,b,c,a*b*c))
+            ans = c
             break
     if flag:
         break
+
+diff_time = timer()
+# 425.0 in 0.614862781375 seconds
+print("{0} in {1} seconds".format(ans, diff_time))
